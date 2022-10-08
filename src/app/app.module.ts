ab492ff0eb41c49df5b59f57ba428849787f8620
percyers <percyers@outlook.com>
2022-10-08 16:35:53 -0300

    first draft, all components created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,26 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './comp/header/header.component';
+import { BannercardComponent } from './comp/bannercard/bannercard.component';
+import { AboutComponent } from './comp/about/about.component';
+import { ExperienceComponent } from './comp/experience/experience.component';
+import { EducationComponent } from './comp/education/education.component';
+import { SkillsComponent } from './comp/skills/skills.component';
+import { AchievementsComponent } from './comp/achievements/achievements.component';
+import { FooterComponent } from './comp/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    BannercardComponent,
+    AboutComponent,
+    ExperienceComponent,
+    EducationComponent,
+    SkillsComponent,
+    AchievementsComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/comp/bannercard/bannercard.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-bannercard',
+  templateUrl: './bannercard.component.html',
+  styleUrls: ['./bannercard.component.css']
+})
+export class BannercardComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
