4f6c26a3fd2261355ec7bff78560e87f085e7ecc
percy <percyers@outlook.com>
2022-12-05 10:22:48 -0300

    cleanups, finishing touches before creating forms

---
+++ src/assets/data/entity.ts
@@ -0,0 +1,37 @@
+export interface User {
+  id: number;
+  name: string;
+  profileBg: string;
+  profilePic: string;
+  currentPosition: string;
+  location: string;
+  about: string;
+}
+
+export interface Experience {
+  id: number;
+  name: string;
+  position: string;
+  timeStart: number;
+  timeEnd: number;
+  location: string;
+  img: string;
+  url: string;
+}
+
+export interface Education {
+  id: number;
+  name: string;
+  title: string;
+  timeStart: number;
+  timeEnd: number;
+  location: string;
+  img: string;
+  url: string;
+}
+
+export interface Skill {
+  id: number;
+  name: string;
+  level: number;
+}
